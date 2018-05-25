1881c2b7cfb77c7d7bead166f07a4dc79988909e
Mariya <mariqvalkova@gmail.com>
2018-05-25 09:45:42 +0300

    NumbersDivisibleBy3
    Numbers from 0 to 100, divisible by 3

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NumbersDivisibleBy3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            for (int i = 3; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                  Console.WriteLine(i);  
+                }
+                
+            }
+            
+        }
+    }
+}
